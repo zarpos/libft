93ebe6d9e3ebf7900011dad46cf1b8e9d8e5a8c4
Diego Rubio morato <drubio-m@c3r11s3.42madrid.com>
2022-03-30 02:48:44 +0200

    29-03-2022

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: drubio-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 18:41:23 by drubio-m          #+#    #+#             */
-/*   Updated: 2022/03/28 23:04:07 by drubio-m         ###   ########.fr       */
+/*   Updated: 2022/03/29 22:48:17 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-void    *ft_memchr(const void *s, int c, size_t n);
-int 	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*strnstr(const char *haystack, const char *needle, size_t len);
+int 	ift_atoi(const char *str);
 #endif
--- makefile
+++ makefile
@@ -6,14 +6,15 @@
 #    By: drubio-m <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/25 20:18:27 by drubio-m          #+#    #+#              #
-#    Updated: 2022/03/28 22:45:35 by drubio-m         ###   ########.fr        #
+#    Updated: 2022/03/29 22:48:48 by drubio-m         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRC = ft_bzero.c ft_isalpha.c ft_isdigit.c ft_memcpy.c ft_memset.c \
 	  ft_strlcat.c ft_strlen.c ft_strrchr.c ft_toupper.c ft_isalnum.c \
 	  ft_isascii.c ft_isprint.c ft_memmove.c ft_strchr.c ft_strlcpy.c \
-	  ft_strncmp.c ft_tolower.c ft_memchr.c ft_memcmp.c 
+	  ft_strncmp.c ft_tolower.c ft_memchr.c ft_memcmp.c ft_strnstr.c \
+	  ft_atoi.c
 OBJS = $(SRC:.c=.o)
 C = gcc -c
 FLAGS = -Wall -Wextra -Werror
